3199380de3424d7988a37eab9be67184ab6c0bb1
Orry Verducci <orry@orryverducci.co.uk>
2021-05-08 23:36:24 +0100

    Version assets on production builds

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,3 +2,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/css/app.scss', 'public/css');
+
+if (mix.inProduction()) {
+    mix.version();
+}
