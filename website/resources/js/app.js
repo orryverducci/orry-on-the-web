471b6ce901c7fa103557b6981ed74ada4aae5770
Orry Verducci <orry@orryverducci.co.uk>
2021-12-29 21:29:03 +0000

    Write intro to console

--- website/resources/js/app.js
+++ website/resources/js/app.js
@@ -1,6 +1,20 @@
 import * as Turbo from "@hotwired/turbo";
 import { Application } from "@hotwired/stimulus"
 import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
+import figlet from "figlet";
+import standard from "figlet/importable-fonts/Standard.js";
+
+figlet.parseFont('Standard', standard);
+
+figlet.text("Hello!", { font: "Standard" }, function(err, data) {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.info(data);
+    console.info("I see you there, poking around the code ;)");
+    console.info("Want to see the source code for this site? Head over to https://github.com/orryverducci/orry-on-the-web");
+});
 
 const application = Application.start();
 const context = require.context("./controllers", true, /\.js$/);
