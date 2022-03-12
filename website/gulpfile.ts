f5a0465665fe2f60ccd7b6688d575027ef64e9f9
Orry Verducci <orry@orryverducci.co.uk>
2022-03-12 16:08:09 +0000

    Minify on production and staging

--- website/gulpfile.ts
+++ website/gulpfile.ts
@@ -6,34 +6,44 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import nodePolyfills from "rollup-plugin-polyfill-node";
 import typescript from "@rollup/plugin-typescript";
+import { terser } from "rollup-plugin-terser";
 import source from "vinyl-source-stream";
 
 const sass = gulpSass(dartSass);
 
 function buildcss() {
+    let style = process.env.NODE_ENV == "production" ? "compressed" as const : "expanded" as const;
+
     return src("./resources/css/app.scss")
         .pipe(sass({
-            includePaths: ["node_modules"]
+            includePaths: ["node_modules"],
+            outputStyle: style
         })
         .on("error", sass.logError))
         .pipe(dest("./public/css"));
 }
 
 function buildjs() {
+    let rollupPlugins = [
+        typescript({
+            compilerOptions: {
+                lib: ["es5", "es6", "dom"],
+                moduleResolution: "node",
+                target: "es6"
+            }
+        }),
+        commonjs(),
+        nodePolyfills(),
+        nodeResolve()
+    ];
+
+    if (process.env.NODE_ENV == "production") {
+        rollupPlugins.push(terser());
+    }
+
     return rollup({
         input: "./resources/js/app.ts",
-        plugins: [
-            typescript({
-                compilerOptions: {
-                    lib: ["es5", "es6", "dom"],
-                    moduleResolution: "node",
-                    target: "es6"
-                }
-            }),
-            commonjs(),
-            nodePolyfills(),
-            nodeResolve()
-        ],
+        plugins: rollupPlugins,
         output: {
             format: "iife"
         }
