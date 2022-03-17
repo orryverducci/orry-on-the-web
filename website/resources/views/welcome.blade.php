e8863c8d1dc456f5595c569cf1b502a9a18affef
Orry Verducci <orry@orryverducci.co.uk>
2022-03-17 15:45:00 +0000

    Add home page

--- website/resources/views/welcome.blade.php
+++ website/resources/views/welcome.blade.php
@@ -1,3 +1,4 @@
-<x-layout class="template-home">
-    
+<x-layout class="template-home header-white">
+    <h1>Hi, I'm Orry</h1>
+    <p><span>Welcome to my corner</span> <span>of the internet</span></p>
 </x-layout>
