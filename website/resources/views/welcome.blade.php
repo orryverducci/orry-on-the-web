d6ce512f1bb80f0e4bc37927af93c52728c2eee4
Orry Verducci <orry@orryverducci.co.uk>
2022-03-21 17:58:45 +0000

    Make header white by default

--- website/resources/views/welcome.blade.php
+++ website/resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<x-layout class="template-home header-white">
+<x-layout class="template-home">
     <h1>Hi, I'm Orry</h1>
     <p><span>Welcome to my corner</span> <span>of the internet</span></p>
 </x-layout>
