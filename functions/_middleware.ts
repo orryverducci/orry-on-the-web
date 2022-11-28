d13c3c91af52eedd8aa16f0cfc98953091c71a36
Orry Verducci <orry@orryverducci.co.uk>
2022-11-28 17:10:24 +0000

    Use middleware to send headers

---
+++ functions/_middleware.ts
@@ -0,0 +1,43 @@
+async function addHeaders(context: EventContext<unknown, string, Record<string, unknown>>): Promise<Response> {
+    // Get the response
+    let response: Response = await context.next();
+
+    // Return the response if it's not a successful one
+    if (response.status != 200) {
+        return response;
+    }
+
+    // Get the request URL
+    let url: URL = new URL(context.request.url);
+
+    // Determine if accessed from production site domains and add a no robots header if not
+    let production: boolean = false;
+
+    if (url.hostname == "orryverducci.co.uk" || url.hostname == "www.orryverducci.co.uk") {
+        production = true;
+    } else {
+        response.headers.append("X-Robots-Tag", "noindex");
+    }
+
+    // Add cache and robots headers on requests for assets
+    if (url.pathname !== undefined && (url.pathname.startsWith("/assets") || url.pathname.startsWith("/fonts") || url.pathname.startsWith("/images"))) {
+        response.headers.append("Cache-Control", "public");
+
+        if (production) {
+            response.headers.append("X-Robots-Tag", "nosnippet");
+        }
+    }
+
+    // Add the security headers
+    if (response.headers.has("Content-Type") && response.headers.get("Content-Type").includes("text/html")) {
+        response.headers.append("Content-Security-Policy", "default-src 'self'; style-src 'self' 'sha256-rql2tlBWA4Hb3HHbUfw797urk+ifBd6EAovoOUGt0oI='; script-src 'self' 'unsafe-eval' static.cloudflareinsights.com; connect-src 'self' cloudflareinsights.com; child-src 'none'; frame-ancestors 'none';");
+    }
+
+    response.headers.append("X-Content-Type-Options", "nosniff");
+    response.headers.append("X-Frame-Options" , "DENY");
+
+    // Return the modified response
+    return response;
+}
+
+export const onRequest: Array<PagesFunction> = [addHeaders];
