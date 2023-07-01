27d4e9475b3f95b801040bb6f397909d8e48902d
Orry Verducci <orry@orryverducci.co.uk>
2023-07-01 18:41:15 +0100

    Switch to new hybrid output mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,10 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
     site: "https://orryverducci.co.uk",
-    output: "server",
+    output: "hybrid",
     adapter: cloudflare({
       mode: "directory"
     }),
-    experimental: {
-      prerender: true
-    },
     vite: {
         build: {
           sourcemap: true,
