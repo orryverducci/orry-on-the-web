a178ff0a59084817b287636864c9dde91da5b4d9
Orry Verducci <orry@orryverducci.co.uk>
2025-01-02 14:29:51 +0000

    Remove depricated hybrid render mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
     site: "https://orryverducci.co.uk",
-    output: "hybrid",
     adapter: cloudflare({
       mode: "directory"
     }),
