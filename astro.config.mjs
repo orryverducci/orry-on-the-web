9036984f75b80114dd532c812a3f5d136b083cd5
Orry Verducci <orry@orryverducci.co.uk>
2026-09-26 21:14:37 +0100

    Use Astro font functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import cloudflare from "@astrojs/cloudflare";
 import icon from "@dallay/astro-icon";
@@ -10,6 +10,40 @@ export default defineConfig({
       mode: "directory"
     }),
     integrations: [icon()],
+    fonts: [
+      {
+        provider: fontProviders.fontsource(),
+        name: "Figtree",
+        cssVariable: "--font-figtree",
+        weights: ["400 600"]
+      },
+      {
+        provider: fontProviders.local(),
+        name: "VCROSDMono",
+        cssVariable: "--font-vcrosd",
+        fallbacks: ["monospace"],
+        options: {
+          variants: [{
+            src: ['./src/fonts/VCROSDMono.woff2', './src/fonts/VCROSDMono.woff'],
+            weight: 'normal',
+            style: 'normal'
+          }]
+        }
+      },
+      {
+        provider: fontProviders.local(),
+        name: "ModeSeven",
+        cssVariable: "--font-modeseven",
+        fallbacks: ["monospace"],
+        options: {
+          variants: [{
+            src: ['./src/fonts/ModeSeven.woff2', './src/fonts/ModeSeven.woff'],
+            weight: 'normal',
+            style: 'normal'
+          }]
+        }
+      }
+    ],
     image: {
       service: {
         entrypoint: 'astro/assets/services/noop'
